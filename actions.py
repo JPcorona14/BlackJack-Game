6c0ac0dcebed4b0968f4254ee47570d703fc0d8a
J <j.pcorona14@gmail.com>
2020-07-20 00:02:05 -0700

    Completed

--- actions.py
+++ actions.py
@@ -10,34 +10,36 @@ def deal(p1, p2):
     p1.add_card(Draw_Card.deal_one())
     p2.add_card(Draw_Card.deal_one())
     hand = 0
-    for card in p1.current_hand:
-        print(f"{p1.name} has a {card}")
-        hand += card.value
-    print(f"\n{p2.name} has a {p2.current_hand[0]}")
+    #    for card in p1.current_hand:
+    #        print(f"{p1.name} has a {card}")
+    #        hand += card.value
+    #   print(f"\n{p2.name} has a {p2.current_hand[0]}")
     return '\n'
 
 
-def game_on(player):
+def draw(p1):
+    p1.add_card(Draw_Card.deal_one())
+
+
+def game_on(player, dealer_name):
     hand = 0
     while True:
         start = True
         hand = 0
         while start:
             for card in player.current_hand:
-                if len(player.current_hand) >= 2:
-                    pass
-                else:
-                    print(f"{player.name} has a {card}")
+                print(f"{player.name} has a {card}")
                 hand += card.value
-                if card.rank == 'Ace' and hand > 21:
-                    hand -= 10
+            if any('Ace' in card.rank for card in player.current_hand) and hand > 21:
+                hand -= 10
             start = False
             print(f"{player.name}'s Total: {hand}\n")
+            print(f"\n{dealer_name.name} has a {dealer_name.current_hand[0]}")
         if hand > 21:
-            return "Bust!"
+            hand = "Bust!"
             break
         elif hand == 21:
-            return "BlackJack!"
+            hand = "BlackJack!"
             break
         hit_or_stay = input('Would you like another card? (Y/N)\n').lower()
         if hit_or_stay == 'y':
@@ -51,20 +53,25 @@ def bank(cash):
     return cash
 
 
-def dealer_on(player):
-    start = True
-    hand = 0
-    player.add_card(Draw_Card.deal_one())
-    while start:
+def dealer_on(player, opponent):
+    turn = 0
+    while True:
+        hand = 0
+        player.add_card(Draw_Card.deal_one())
+        last_card = player.current_hand[-1]
         for card in player.current_hand:
             print(f"{player.name} has a {card}")
             hand += card.value
-            if card.rank == 'Ace' and hand > 21:
+            turn += 1
+            if any('Ace' in card.rank for card in player.current_hand) and hand > 21:
                 hand -= 10
-        start = False
         print(f"{player.name}'s Total: {hand}\n")
-    if hand > 21:
-        return "Bust!"
-    elif hand == 21:
-        return "BlackJack!"
+        if hand > 21:
+            hand = "Bust!"
+            break
+        elif hand == 21:
+            hand = "BlackJack!"
+            break
+        elif hand > opponent:
+            break
     return hand
--- logic.py
+++ logic.py
@@ -6,55 +6,75 @@ from actions import bank
 
 # GAME LOGIC STARTS HERE
 
-Player1 = Player(input("Enter you Name:"))
+Player1 = Player(input("Enter you Name:\n"))
 Dealer1 = Player('Dealer')
-Losing = False
+champ = True
 
 # player_turn = game_on(Player1)
 # dealer_turn = Dealer1.current_hand[0].value + Dealer1.current_hand[1].value
 
+game_turns = 0
 while True:
-    game_turns = 0
     if game_turns == 0:
-        Current_Bank = bank(1000)
-        #player_bank = Player1.bank(Current_Bank)
+        Current_Bank = bank(int(input("How much would you like to play with? \n")))
         print(f"{Player1.name} is entering the game with ${Current_Bank}\n")
-        print(deal(Player1, Dealer1))
 
-    """
     while True:
-        bett = int(input("What is your bet for this hand?\n"))
-        player_turn = game_on(Player1)
-        dealer_turn = dealer_on(Dealer1)
+        print(deal(Player1, Dealer1))
+        bet = int(input("What is your bet for this hand?\n"))
+        if bet > Current_Bank:
+            print("Insufficient Amount, Please enter new amount to bet!")
+            bet = int(input("What is your bet for this hand?\n"))
+        player_turn = game_on(Player1, Dealer1)
+        dealer_hand = Dealer1.current_hand[0].value + Dealer1.current_hand[1].value
+
         if player_turn == "Bust!":
             print(f'{Player1.name} Bust! \nDealer Wins!')
+            champ = False
             break
         elif player_turn == "BlackJack!":
             print(f'{Player1.name} Wins with BlackJack!')
+            champ = True
             break
-        elif player_turn > dealer_turn:
-            while True:
-                if dealer_turn == "Bust!":
-                    print(f"{Player1.name} Wins!")
-                    break
-                elif dealer_turn == "BlackJack!":
-                    print(f"{Dealer1.name} Wins with BlackJack!")
-                    break
-                else:
-                    print(dealer_on(Dealer1))
+        elif player_turn >= dealer_hand:
+            dealer_turn = dealer_on(Dealer1, player_turn)
+            if dealer_turn == "Bust!":
+                print(f"{Player1.name} Wins! Dealer Bust!")
+                champ = True
+                break
+            elif dealer_turn == "BlackJack!":
+                print(f"{Dealer1.name} Wins with BlackJack!")
+                champ = False
+                break
+            elif player_turn < dealer_hand:
+                print(f"Dealer wins with a total of {dealer_hand}!")
+                champ = False
+                break
+        elif player_turn < dealer_hand:
+            print(f"Dealer wins with a total of {dealer_hand}!")
+            champ = False
             break
-
-        if dealer_turn == "Bust!":
-            print(f"{Player1.name} Wins!")
-        elif dealer_turn == "BlackJack!":
-            print(f"{Dealer1.name} Wins with BlackJack!")
         else:
-            print(
-                f"{Dealer1.name} Wins with {Dealer1.current_hand[0]} & {Dealer1.current_hand[1]}")
-    continue_playing = input("Would you like to play again? (Y/N)\n").lower
-    if continue_playing == "y":
-        game_turns += 1
+            print("Other outcome not specified!")
+    if champ:
+        Current_Bank += bet
     else:
+        Current_Bank -= bet
+        if Current_Bank <= 0:
+            print('Game Over: No more funds')
+            break
+    print(f"Your current bank is:{Current_Bank}")
+    continue_playing = input("Would you like to play again? (Y/N)\n").lower()
+    #    print(continue_playing)
+    while True:
+        if continue_playing == "y" or continue_playing == "n":
+            break
+        print("Please enter a proper selection: Y for yes or N for no")
+        continue_playing = input("Would you like to play again? (Y/N)\n").lower()
+    if continue_playing == "y":
+        Player1.current_hand = []
+        Dealer1.current_hand = []
+        game_turns += 1
+    elif continue_playing == "n":
+        print("Thank you for Playing!")
         break
-    """
-    break
